042c376697a06ab4650ed04108fc09097a4c5000
DGSENZEN <diegogaytan2000@gmail.com>
2024-08-18 13:53:42 -0500

    8/18/2024, finished poor man's bar v1, make sure to clean for commas and apostrophes

--- poor_mans_bar.py
+++ poor_mans_bar.py
@@ -10,20 +10,33 @@ def generate_dict():
         if letter not in dicti:
             dicti[letter] = []
         else:
-            print("Already exists")
-
+            raise Exception("Already exists")
     return dicti
 
-def process_words(sentence, generate_dict):
-    return None #to be completed, should process the characters of a word and then append that list towards the dictionary generated
+def populate_dict(sentence):
+    """Populates the dictionary with the processed sentence"""
+    sentence = sentence.strip('!,.?')
+    words = sentence.split()
+    pm_bar = generate_dict()
+    for word in words:
+        for char in word:
+            if char not in pm_bar and char.isalpha():
+                pm_bar[char].append(char)
+            elif char.isalpha():
+                pm_bar[char].append(char)
+            elif not char.isalpha():
+                raise Exception("Only valid characters allowed.")
+            
+    return pm_bar
+#to be completed, should process the characters of a word and then append that list towards the dictionary generated
 
 
 def main():
     """Main function."""
     print("--Poor Man's Bar--")
     sentence = input("Please input your sentence: \n")
-    print(generate_dict())
-
+    result = populate_dict(sentence.lower())
+    print(result)
 
 if __name__ == "__main__":
     main()
